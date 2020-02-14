16b2090ed27d6cd2fcb57110047e3c1737779423
2929732416 <2929732416@qq.com>
2020-02-14 15:47:23 +0800

    添加工具类

---
+++ ConsoleApp_GitTest/Tool.cs
@@ -0,0 +1,36 @@
+﻿/*
+* ==================================================================
+*	Copyright (C) 2020
+*
+*	描述：Tool
+*
+*	作者：DJ
+*
+*	版本：1.0
+*
+*	CLR 版本：4.0.30319.42000
+*
+*	创建时间：2020/2/14 15:46:44
+*
+* ==================================================================
+*	
+*	命名空间：ConsoleApp_GitTest
+*
+*	类名：Tool
+*	
+* ==================================================================
+*/
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_GitTest
+{
+    class Tool
+    {
+    }
+}
